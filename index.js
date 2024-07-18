6219fe2a5027a2f0fe117f8cde615bf335a4019b
jainefer <jy5849@naver.com>
2024-07-18 17:24:11 +0900

    [feat]초기세팅

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.json());                    // request의 본문을 json으로 �
 app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
 
 
-
+//route 추가하는 칸
 app.use('/sample',sampleRoute);
 
 
