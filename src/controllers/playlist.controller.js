3ae421ea6bc6a0d43625b9cc9d56f8cfebafb9de
Paul Kang <kminh1209@naver.com>
2024-08-05 14:53:34 +0900

    playlist.controller 추가
    playlist.controller 추가 & playlist.service 불필요한 코드 삭제

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,28 @@
+// 필요한 모듈과 서비스 함수를 가져옵니다.
+import { BaseError } from "../../config/error.js";
+import { response } from "../../config/response.js";
+import { status } from "../../config/response.status.js";
+import { insertPlaylistService } from "../services/playlist.service.js";
+import { PlaylistInsertRequestDTO } from "../dtos/playlist.dto.js";
+
+// controller 함수
+export const insertPlaylistController = async (req, res, next) => {
+    try {
+        const time = new Date()
+        // 토큰에서 사용자 ID를 복호화 하기 위해 필요한 코드(필요시 주석 해제)
+        // const token = req.headers.authorization.split(' ')[1];
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = 1; // 임시로 고정된 사용자 ID를 사용합니다.
+
+        // 요청 데이터를 DTO를 통해 가공
+        const requestData = PlaylistInsertRequestDTO(userId, req.body, time);
+        console.log("플레이리스트 생성 컨트롤러 작동", requestData);
+
+        // insertPlayListService 함수 통해 플레이리스트를 생성
+        res.send(response(status.SUCCESS, await insertPlaylistService(requestData)));
+
+    } catch (error) { // 에러 처리
+        console.error(error);
+        res.send(response(status.BAD_REQUEST, BaseError(status.BAD_REQUEST)));
+    }
+};
--- src/services/playlist.service.js
+++ src/services/playlist.service.js
@@ -4,18 +4,10 @@ import { status } from "../../config/response.status.js";
 import { insertPlaylistDAO } from "../models/dao/playlist.dao.js";
 
 // 플레이리스트를 생성하는 함수
-export const insertPlaylistService = async (userId, data) => {
-    // 플레이리스트 생성 요청 데이터를 DTO를 통해 가공합니다.
-    const time = new Date().toISOString();
-    const playlistData = {
-        id: userId,
-        title: data.title,
-        createAt: time
-    };
-
+export const insertPlaylistService = async (requestData) => {
     // DAO 함수통해 DB에 위에 playlistData 삽입
     try {
-        const insertPlaylistData = await insertPlaylistDAO(playlistData);
+        const insertPlaylistData = await insertPlaylistDAO(requestData);
         return insertPlaylistData;
     } catch (error) { // 예외처리
         console.error(error);
