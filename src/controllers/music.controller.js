f51168b49d2b0895460abdb96632607cf26da499
hardNormalEasyMode <petequdwns12@naver.com>
2024-08-06 19:44:14 +0900

    music.change-info.api fix 2
    music.change-info.api fix 2
    
    add change info api in route.js, controller.js

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -2,8 +2,8 @@ import { BaseError } from "../../config/error.js";
 import { response } from "../../config/response.js";
 import { status } from "../../config/response.status.js";
 import  jwt  from "jsonwebtoken";
-import { insertMusicService } from "../services/music.service.js";
-import { MusicInsertRequestDTO } from "../dtos/music.dto.js";
+import { insertMusicService ,changeinfoMusicService} from "../services/music.service.js";
+import { MusicInsertRequestDTO ,MusicChangeinfoRequestDTO} from "../dtos/music.dto.js";
 export const insertMusicController=async(req,res,next)=>{
     try{
         const time = new Date;
@@ -13,6 +13,20 @@ export const insertMusicController=async(req,res,next)=>{
         const requestData= MusicInsertRequestDTO(userId,req.body,time);
         console.log("컨트롤러 작동",requestData);
         res.send(response(status.SUCCESS,await insertMusicService(requestData)));
+    }catch(error){
+        console.error(error);
+        res.send(response.BaseError(status.BAD_REQUEST));
+    }
+}
+export const changeinfoMusicController=async(req,res,next)=>{
+    try{
+        const time = new Date;
+        // const token = req.headers.authorization.split(' ')[1];
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = 1
+        const requestData= MusicChangeinfoRequestDTO(userId,req.body,time);
+        console.log("컨트롤러 작동",requestData);
+        res.send(response(status.SUCCESS,await changeinfoMusicService(requestData)));
     }catch(error){
         console.error(error);
         res.send(response.BaseError(status.BAD_REQUEST));
--- src/routes/music.route.js
+++ src/routes/music.route.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { insertMusicController } from "../controllers/music.controller";
+import { insertMusicController ,changeinfoMusicControler} from "../controllers/music.controller";
 export const musicRoute= express.Router();
 
 musicRoute.post('',async(req,res)=>{
     const result = await insertMusicController(req,res);
 })
+musicRoute.post('/change-info',async(req,res)=>{
+    const result = await changeinfoMusicControler(req,res);
+}
+)
