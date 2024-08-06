391e995f0f0c1c11c8c41801201cf8402f793168
hardNormalEasyMode <petequdwns12@naver.com>
2024-08-06 15:35:24 +0900

    api fix 1
    change-info api fix1

--- src/routes/music.change-info.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { changeinfoMusicController } from "../controllers/music.change-info.controller";
-export const musicchangeinfoRoute= express.Router();
-
-musicchangeinfoRoute.post('/change-info',async(req,res)=>{
-    const result = await changeinfoMusicController(req,res);
-})
