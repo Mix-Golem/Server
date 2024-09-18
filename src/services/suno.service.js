9b351a2a1f119b0e4c855f141d61ab065948bcc9
jainefer <jy5849@naver.com>
2024-09-18 17:15:26 +0900

    [feat] 음악생성

--- src/services/suno.service.js
+++ src/services/suno.service.js
@@ -6,17 +6,21 @@ import { sunoResponseDTO } from "../dtos/suno.dto";
 const baseUrl= "http://suno.mixgolem.site"
 
 export const sunoService=async (req)=>{
-    
-    const url=`${baseUrl}/api/generate`;
-    const response = await axios.post(url,{
-        "prompt":req.prompt,
-        "make_instrumental": false,
-        "wait_audio": true
-    },{
-        headers : {"Content-Type": "application/json"},
-    });
-    console.log(response.data);
+    try{
+        const url=`${baseUrl}/api/generate`;
+        const response = await axios.post(url,{
+            "prompt":req.prompt,
+            "make_instrumental": false,
+            "wait_audio": true
+        },{
+            headers : {"Content-Type": "application/json"},
+        });
+        console.log(response.data);
 
-    return sunoResponseDTO(response.data[0]);
+        return sunoResponseDTO(response.data[0]);
+    }catch(error){
+        console.error(error);
+    }
+    
 
 };
