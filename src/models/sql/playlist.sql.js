ea97e68765bb6de3b5c83c574f813fea70ba881a
Paul Kang <kminh1209@naver.com>
2024-11-05 15:09:16 +0900

    피드백 반영
    Issue1. 플레이리스트를 조회했을 때, thumnail이 mp3파일로 나옴
    - Mp3 파일이 아닌 SONG_INFO에서 thumbnail 불러오는 걸로 수정
    - Reponse body에 artist_name 추가될 수 있게 수정
    
    Issue2. 플레이리스트를 만들고 나면 null 값을 갖는 song 데이터가 reponse body에 포함된다.
    - Playlist 생성시 후, 속에 노래가 없을 경우 플레이리스트 조회를 했을때 보여지지 않음

--- src/models/sql/playlist.sql.js
+++ src/models/sql/playlist.sql.js
@@ -39,7 +39,7 @@ export const playlistInfoSql = `
         sp.id AS playlist_id, 
         sp.title AS playlist_title, 
         (
-            SELECT si.media 
+            SELECT si.thumbnail
             FROM SONG_INFO_TB si
             JOIN SONG_PLAYLIST_INFO spi ON si.id = spi.song_id
             WHERE spi.playlist_id = sp.id
@@ -50,19 +50,20 @@ export const playlistInfoSql = `
             JSON_OBJECT(
                 'song_id', si.id, 
                 'song_title', si.title, 
-                'song_order', spi.order,
+                'song_order', spi.order,  
                 'artist_id', um.id,
                 'artist_name', um.name,
-                'thumbnail', si.media
+                'thumbnail', si.thumbnail
             )
         ) AS songs
     FROM SONG_PLAYLIST_TB sp
     LEFT JOIN SONG_PLAYLIST_INFO spi ON sp.id = spi.playlist_id
     LEFT JOIN SONG_INFO_TB si ON spi.song_id = si.id
     LEFT JOIN USER_MEMBER_TB um ON si.user_id = um.id
-    WHERE sp.id = ?
+    WHERE sp.id = ? AND si.id IS NOT NULL
     GROUP BY sp.id;
 `;
+
 
 
 
